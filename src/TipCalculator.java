6802ca1bea568514d860b115b3f2f25c0d84dea1
1E10 <sample@bths.edu>
2022-10-09 13:17:48 -0400

    Refactored

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -2,20 +2,56 @@ public class TipCalculator {
 
     //instance variables
     private int people;
-    private double price;
     private int percent;
-    private double accumulatedBill;
+    private double totalBillBeforeTip;
 
+
+    //constructor
     public TipCalculator (int people, int percent) {
         this.people = people;
         this.percent = percent;
-        price = 0;
-        accumulatedBill = 0;
+        totalBillBeforeTip = 0.0;
     }
 
-    public void totalBill (double price) {
-        accumulatedBill += price;
+    //method that increments total bill before tip
+    public double addMeal(double cost) {
+        totalBillBeforeTip += cost;
+        return totalBillBeforeTip;
     }
 
+    //getter method that returns the total bill before tip
+    public double getTotalBillBeforeTip(){
+        return totalBillBeforeTip;
+    }
 
+    //getter method that returns the tip
+    public int getTipPercent(){
+        return percent;
+    }
+
+    //method that calculates and returns a double representing th total tip amount
+    public double tipAmount(){
+        return totalBillBeforeTip * (double)percent / 100;
+
+    }
+
+    //method that calculates and returns a double representing the total bill WITH tip
+    public double totalBill() {
+        return totalBillBeforeTip + tipAmount();
+    }
+
+    //method that calculates and returns a double representing per person cost before tip
+    public double perPersonCostBeforeTip() {
+        return totalBillBeforeTip/people;
+    }
+
+    //method that calculates and returns a double representing per person tip amount
+    public double perPersonTipAmount() {
+        return tipAmount()/people;
+    }
+
+    //method that calculates and returns a double representing the total cost per person
+    public double perPersonTotalCost() {
+        return totalBill()/people;
+    }
 }
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,44 +1,71 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
+
 public class TipCalculatorRunner {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Scanner scan = new Scanner(System.in);
-            DecimalFormat formatter = new DecimalFormat("#.##");
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat formatter = new DecimalFormat("#.##");
 
-            System.out.println("----------------------------------");
-            System.out.println("| Welcome to the Tip Calculator! |");
-            System.out.println("----------------------------------");
-            System.out.print("How many people are in your group? ");
-            int totalPeople = scan.nextInt();
+        //introduction
+        System.out.println("----------------------------------");
+        System.out.println("| Welcome to the Tip Calculator! |");
+        System.out.println("----------------------------------");
+        System.out.print("How many people are in your group? ");
+        int totalPeople = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("What's the tip percentage? (1-100) ");
+        int tipPercent = scan.nextInt();
+        scan.nextLine();
+
+        //creates the tip calculator object
+        TipCalculator calculator = new TipCalculator(totalPeople, tipPercent);
+
+        //variables for the while loop//
+        double updatePrice = 0;
+
+        //iterates until the user types in -1
+        while (updatePrice >=0) {
+            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (Type -1 to end): ");
+            calculator.addMeal(updatePrice);
+            updatePrice = scan.nextDouble();
             scan.nextLine();
-
-            System.out.print("What's the tip percentage? (1-100) ");
-            int tipPercent = scan.nextInt();
-            scan.nextLine();
-
-            //tip calculator object
-            TipCalculator calc = new TipCalculator(totalPeople, tipPercent);
-
-            //variables for the while loop
-            double totalBill = 0;
-            double updatePrice = 0;
-
-            //while loop that will iterate until the user types in -1
-            while (updatePrice >=0) {
-                System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (Type -1 to end): ");
-                totalBill += updatePrice;
-                updatePrice = scan.nextDouble();
-            }
-
-            //receipt
-
-
-
-
-
-
         }
+
+        //prints total bill before tip + tip percentage
+        System.out.println("---------------------------------------");
+        String formattedBillBefore = formatter.format(calculator.getTotalBillBeforeTip());
+        System.out.println("Total bill before tip: " + "$" + formattedBillBefore);
+        System.out.println("Tip percentage: " + calculator.getTipPercent() + "%");
+
+
+        //prints tip variable
+        String formattedTip = formatter.format(calculator.tipAmount());
+        System.out.println("Total Tip: " + "$" + formattedTip);
+
+        //prints total bill with tip
+        String formattedBill = formatter.format(calculator.totalBill());
+        System.out.println("Total Bill with tip: " + "$" + formattedBill);
+
+        //prints total bill before tip divided amongst totalPeople
+        String formattedCost = formatter.format(calculator.perPersonCostBeforeTip());
+        System.out.println("Per person cost before tip: " + "$" + formattedCost);
+
+        //prints tip per person
+        String formattedTipPer = formatter.format(calculator.perPersonTipAmount());
+        System.out.println("Tip per person: " + "$" + formattedTipPer);
+
+        //prints total cost per person
+        String formattedTotalPer = formatter.format(calculator.perPersonTotalCost());
+        System.out.println("Total cost per person: " + "$" + formattedTotalPer);
+        System.out.println("---------------------------------------");
+
+        //prints farewell
+        System.out.println("Thank you for using the Tip Calculator!");
+        scan.close();
+
     }
+}
 
 
